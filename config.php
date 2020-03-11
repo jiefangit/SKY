ca17a13dd429c18a8d1fd2dac01ea9c07d15f6fa
ITCODE <42146796+jiefangit@users.noreply.github.com>
2020-03-12 00:45:37 +0800

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,9 @@ header ("content-type:text/html;charset=utf8");
 
 //===========定义常量============
 //用户申请应用获取的值
-define("App_Key","600102237");
-define("App_Secret","4cae2c68b577bc977620e003a2bef4bc");
+//define("App_Key","600102237");
+//define("App_Secret","4cae2c68b577bc977620e003a2bef4bc");
+define("App_Key","389571370000274819");
+define("App_Secret","cee154337204a077e8928a0d7c8ddb49");
 //自定义回调地址
-define("WB_CALLBACK_URL","http://www.sky.com/callback.php");
+define("WB_CALLBACK_URL","https://ctyun.herokuapp.com/callback.php");
